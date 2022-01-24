2c88788a967a4ef2cbc0d5a4b2492ba81bc7dec3
zehaoj <zehao323@gmail.com>
2022-01-24 14:20:02 +0100

    Original files

---
+++ Assets/Scrips/CarAI_PD_tracker.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+
+namespace UnityStandardAssets.Vehicles.Car
+{
+    [RequireComponent(typeof(CarController))]
+    public class CarAI_PD_tracker : MonoBehaviour
+    {
+        private CarController m_Car; // the car controller we want to use
+
+        public GameObject terrain_manager_game_object;
+        TerrainManager terrain_manager;
+
+        public GameObject[] friends;
+        public GameObject[] enemies;
+
+        public GameObject my_target;
+        public Vector3 target_velocity;
+        Vector3 old_target_pos;
+        Vector3 desired_velocity;
+
+        public float k_p = 2f;
+        public float k_d = 0.5f;
+
+        Rigidbody my_rigidbody;
+
+        private void Start()
+        {
+            // get the car controller
+            m_Car = GetComponent<CarController>();
+            terrain_manager = terrain_manager_game_object.GetComponent<TerrainManager>();
+
+            my_rigidbody = GetComponent<Rigidbody>();
+
+            old_target_pos = my_target.transform.position;
+
+            // Plan your path here
+            // ...
+        }
+
+
+        private void FixedUpdate()
+        {
+            // keep track of target position and velocity
+            Vector3 target_position = my_target.transform.position;
+            target_velocity = (target_position - old_target_pos) / Time.fixedDeltaTime;
+            old_target_pos = target_position;
+
+            // a PD-controller to get desired velocity
+            Vector3 position_error = target_position - transform.position;
+            Vector3 velocity_error = target_velocity - my_rigidbody.velocity;
+            Vector3 desired_acceleration = k_p * position_error + k_d * velocity_error;
+
+            float steering = Vector3.Dot(desired_acceleration, transform.right);
+            float acceleration = Vector3.Dot(desired_acceleration, transform.forward);
+
+            Debug.DrawLine(target_position, target_position + target_velocity, Color.red);
+            Debug.DrawLine(transform.position, transform.position + my_rigidbody.velocity, Color.blue);
+            Debug.DrawLine(transform.position, transform.position + desired_acceleration, Color.black);
+
+            // this is how you control the car
+            Debug.Log("Steering:" + steering + " Acceleration:" + acceleration);
+            m_Car.Move(steering, acceleration, acceleration, 0f);
+        }
+    }
+}
---
+++ Assets/Scrips/TerrainManager.cs
@@ -0,0 +1,204 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Newtonsoft.Json; // Import JSON.NET from Unity Asset store (not needed in 2022 version)
+
+
+public class TerrainManager : MonoBehaviour {
+
+
+    //public TestScriptNoObject testNoObject = new TestScriptNoObject();
+
+    public string terrain_filename = "Text/terrain";
+    public TerrainInfo myInfo;
+
+    public GameObject flag;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Use this for initialization
+    void Awake()
+    {
+
+        var jsonTextFile = Resources.Load<TextAsset>(terrain_filename);
+
+        // set to false for hard coding new terrain using TerrainInfo2 below
+        bool loadFromFile = true;
+
+        if (loadFromFile)
+        {
+            myInfo = TerrainInfo.CreateFromJSON(jsonTextFile.text);
+        }
+        else
+        {
+            myInfo.TerrainInfo2();
+            //myInfo.file_name = "test88";
+            string myString = myInfo.SaveToString();
+            myInfo.WriteDataToFile(myString);
+        }
+
+        myInfo.CreateCubes();
+
+        Instantiate(flag, myInfo.start_pos, Quaternion.identity);
+        Instantiate(flag, myInfo.goal_pos, Quaternion.identity);
+
+
+
+    }
+
+
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
+
+
+
+[System.Serializable]
+public class TerrainInfo
+{
+    public string file_name;
+    public float x_low;
+    public float x_high;
+    public int x_N;
+    public float z_low;
+    public float z_high;
+    public int z_N;
+    public float[,] traversability;
+
+    public Vector3 start_pos;
+    public Vector3 goal_pos;
+
+    
+    public void TerrainInfo2()
+    {
+        file_name = "my_terrain";
+        x_low = 50f;
+        x_high = 250f;
+        x_N = 45;
+        z_low = 50f;
+        z_high = 250f;
+        z_N = 7;
+
+        start_pos = new Vector3(100f, 1f, 100f);
+        goal_pos = new Vector3(110f, 1f, 110f);
+
+
+        Debug.Log("Using hard coded info...");
+        //traversability = new float[,] { { 1.1f, 2f }, { 3.3f, 4.4f } };
+        traversability = new float[x_N, z_N]; // hardcoded now, needs to change
+        for(int i = 0; i < x_N; i++)
+        {
+            for (int j = 0; j < z_N; j++)
+            {
+                if ((i == 0 || i == x_N -1) || (j == 0 || j == z_N - 1))
+                {
+                    traversability[i, j] = 1.0f;
+                }
+                else
+                {
+                    traversability[i, j] = 0.0f;
+                }
+            }
+        }
+    }
+
+    public int get_i_index(float x)
+    {
+        int index = (int) Mathf.Floor(x_N * (x - x_low) / (x_high - x_low));
+        if (index < 0)
+        {
+            index = 0;
+        }else if (index > x_N - 1)
+        {
+            index = x_N - 1;
+        }
+        return index;
+
+    }
+    public int get_j_index(float z) // get index of given coordinate
+    {
+        int index = (int)Mathf.Floor(z_N * (z - z_low) / (z_high - z_low));
+        if (index < 0)
+        {
+            index = 0;
+        }
+        else if (index > z_N - 1)
+        {
+            index = z_N - 1;
+        }
+        return index;
+    }
+
+    public float get_x_pos(int i)
+    {
+        float step = (x_high - x_low) / x_N;
+        return x_low + step / 2 + step * i;
+    }
+
+    public float get_z_pos(int j) // get position of given index
+    {
+        float step = (z_high - z_low) / z_N;
+        return z_low + step / 2 + step * j;
+    }
+
+    public void CreateCubes()
+    {
+        float x_step = (x_high - x_low) / x_N;
+        float z_step = (z_high - z_low) / z_N;
+        for (int i = 0; i < x_N; i++)
+        {
+            for (int j = 0; j < z_N; j++)
+            {
+                if (traversability[i, j] > 0.5f)
+                {
+                    GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                    cube.transform.position = new Vector3(get_x_pos(i), 0.0f, get_z_pos(j));
+                    cube.transform.localScale = new Vector3(x_step, 15.0f, z_step);
+                }
+
+            }
+        }
+    }
+
+
+
+    public static TerrainInfo CreateFromJSON(string jsonString)
+    {
+        //Debug.Log("Reading json");
+        return JsonConvert.DeserializeObject<TerrainInfo>(jsonString);
+        //return JsonUtility.FromJson<TerrainInfo>(jsonString);
+    }
+
+    public string SaveToString()
+    {
+        JsonSerializerSettings JSS = new JsonSerializerSettings()
+        {
+            ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
+        };
+
+        return JsonConvert.SerializeObject(this, Formatting.None, JSS);
+
+        //return JsonConvert.SerializeObject(this); // throws ref loop error
+        //return JsonUtility.ToJson(this); // cannot handle multi-dim arrays
+    }
+
+    public void WriteDataToFile(string jsonString)
+    {
+        string path = Application.dataPath + "/Resources/Text/" + file_name + ".json";
+        Debug.Log("AssetPath:" + path);
+        System.IO.File.WriteAllText(path, jsonString);
+#if UNITY_EDITOR
+        UnityEditor.AssetDatabase.Refresh();
+#endif
+    }
+
+
+}
