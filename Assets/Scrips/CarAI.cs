adf3d4854037f211bcf5ea6e8c2c8613cf1f218b
santor <sassator95@hotmail.com>
2022-01-27 13:54:07 +0100

    RRT now finds a path (myPath)
    Although it is not an optimal path yet

--- Assets/Scrips/CarAI.cs
+++ Assets/Scrips/CarAI.cs
@@ -23,44 +23,28 @@ namespace UnityStandardAssets.Vehicles.Car
             terrain_manager = terrain_manager_game_object.GetComponent<TerrainManager>();
 
             // Plan your path here
-            // Replace the code below that makes a random path
-            // ...
 
             Vector3 start_pos = terrain_manager.myInfo.start_pos;
             Vector3 goal_pos = terrain_manager.myInfo.goal_pos;
             
             // RRT function
-            Rrt(start_pos, goal_pos);
-            
-            // Replace the code below that makes a random path
-            List<Vector3> my_path = new List<Vector3>();
+            List<Vector3> myPath = Rrt(start_pos, goal_pos);
+            myPath.Reverse();
 
-            my_path.Add(start_pos);
-
-            for (int i = 0; i < 3; i++)
-            {
-                Vector3 waypoint = start_pos + new Vector3(UnityEngine.Random.Range(-50.0f, 50.0f), 0, UnityEngine.Random.Range(-30.0f, 30.0f));
-                my_path.Add(waypoint);
-            }
-            my_path.Add(goal_pos);
-            
             // Plot your path to see if it makes sense
             // Note that path can only be seen in "Scene" window, not "Game" window
             Vector3 old_wp = start_pos;
-            /*foreach (var wp in my_path)
+            foreach (var wp in myPath)
             {
                 Debug.DrawLine(old_wp, wp, Color.red, 100f);
                 old_wp = wp;
-            }*/
-
-            
+            }
         }
 
 
         private void FixedUpdate()
         {
             // Execute your path here
-            // ...
 
             // this is how you access information about the terrain from the map
             int i = terrain_manager.myInfo.get_i_index(transform.position.x);
@@ -79,14 +63,13 @@ namespace UnityStandardAssets.Vehicles.Car
                 Debug.DrawRay(transform.position, closestObstacleInFront, Color.yellow);
                 Debug.Log("Did Hit");
             }
-
-
+            
             // this is how you control the car
             m_Car.Move(1f, 1f, 1f, 0f);
 
         }
 
-        public void Rrt(Vector3 start_pos, Vector3 goal_pos)
+        public List<Vector3> Rrt(Vector3 start_pos, Vector3 goal_pos)
         {
             float xLow = terrain_manager.myInfo.x_low;
             float xHigh = terrain_manager.myInfo.x_high;
@@ -99,6 +82,11 @@ namespace UnityStandardAssets.Vehicles.Car
             var tree = new Node<Vector3>(start_pos);
             Node<Vector3> newParent = tree.Root;
             int iter = 0;
+            bool treeTraversal = true;
+            List<Vector3> myPath = new List<Vector3>();
+            Node<Vector3> finalNode = null;
+            Node<Vector3> currNode;
+            Vector3 parentPoint = start_pos;
             
 
             while (pathFound == false)
@@ -109,7 +97,6 @@ namespace UnityStandardAssets.Vehicles.Car
                 //Debug.DrawLine(start_pos, randomPoint, Color.red, 100f);
                 
                 float dist = Vector3.Distance(start_pos, randomPoint);
-                Vector3 parentPoint = start_pos;
                 foreach (Node<Vector3> node in tree.All)
                 {
                     float newDist = Vector3.Distance(node.Value, randomPoint);
@@ -123,44 +110,51 @@ namespace UnityStandardAssets.Vehicles.Car
                 Vector3 wayPoint = FindWayPoint(parentPoint, randomPoint);
                 //Debug.DrawLine(start_pos, wayPoint, Color.green, 100f);
 
-                int i = terrain_manager.myInfo.get_i_index(wayPoint.x);
-                int j = terrain_manager.myInfo.get_j_index(wayPoint.z);
-                float obstacle = terrain_manager.myInfo.traversability[i, j];
-                if (obstacle == 0.0f)
+                bool onObstacle = CheckObstacleStatus(wayPoint);
+                if (!onObstacle)
                 {
-                    newParent.Add(wayPoint);
+                    currNode = newParent.Add(wayPoint);
                     foreach (var node in tree.All.Values())
                     {
                         Debug.DrawLine(newParent.Value, wayPoint, Color.blue, 100f);
                         
                     }
-                }
-                
-                distanceGoal = Vector3.Distance(wayPoint, goal_pos);
-                if (distanceGoal < distanceThreshold)
-                {
-                    Debug.Log(String.Format("Found goal in {0} iterations.",
-                        iter));
-                    pathFound = true;
-                    
+                    distanceGoal = Vector3.Distance(wayPoint, goal_pos);
+                    if (distanceGoal < distanceThreshold)
+                    {
+                        Debug.Log(String.Format("Found goal in {0} iterations.",
+                            iter));
+                        finalNode = currNode;
+                        pathFound = true;
+
+                    }
                 }
             }
+            // We have found a path to the goal, so now we traverse the tree and find the path nodes
+            while (treeTraversal)
+            {
+                myPath.Add(finalNode.Value);
+                finalNode = finalNode.Parent;
+                if (finalNode == tree.Root)
+                {
+                    myPath.Add(tree.Root.Value);
+                    treeTraversal = false;
+                }
+            }
+
+            return myPath;
         }
 
         public Vector3 FindRandomPoint(float xLow, float xHigh, float zLow, float zHigh)
         {
-            int i;
-            int j;
             Vector3 randomPoint = new Vector3(0,0,0);
-            float obstacle = 1.0f;
+            bool onObstacle = true;
             
-            while (obstacle == 1.0f)
+            while (onObstacle)
             {
                 randomPoint = new Vector3(UnityEngine.Random.Range(xLow, xHigh), 0,
                     UnityEngine.Random.Range(zLow, zHigh));
-                i = terrain_manager.myInfo.get_i_index(randomPoint.x);
-                j = terrain_manager.myInfo.get_j_index(randomPoint.z);
-                obstacle = terrain_manager.myInfo.traversability[i, j];
+                onObstacle = CheckObstacleStatus(randomPoint);
             }
             return randomPoint;
         }
@@ -169,23 +163,28 @@ namespace UnityStandardAssets.Vehicles.Car
         { 
             const int stepSize = 5;
             (float xDistWayPoint, float zDistWayPoint) = GetCoordDistanceBetweenPoints(start_pos, endPoint);
-            float distWayPoint = GetEuclDistanceBetweenPoints(start_pos, endPoint);
+            float distWayPoint = Vector3.Distance(start_pos, endPoint);
             float wayPointX = start_pos.x+(stepSize * xDistWayPoint / distWayPoint);
             float wayPointZ = start_pos.z+(stepSize * zDistWayPoint / distWayPoint);
             Vector3 wayPoint = new Vector3(wayPointX, 0,wayPointZ);
             return wayPoint;
-        }
-
-        public float GetEuclDistanceBetweenPoints(Vector3 pointA, Vector3 pointB)
-        {
-            float pathDistance = Mathf.Sqrt(Mathf.Pow(pointB.x - pointA.x,2) + Mathf.Pow(pointB.z - pointA.z,2));
-            return pathDistance; //Vector3.Distance(pointA, pointB) instead?
         }
         public (float, float) GetCoordDistanceBetweenPoints(Vector3 pointA, Vector3 pointB)
         {
             float xDistance = pointB.x - pointA.x;
             float zDistance = pointB.z - pointA.z;
             return (xDistance, zDistance);
+        }
+        public bool CheckObstacleStatus(Vector3 point)
+        {
+            int i = terrain_manager.myInfo.get_i_index(point.x);
+            int j = terrain_manager.myInfo.get_j_index(point.z);
+            float obstacle = terrain_manager.myInfo.traversability[i, j];// 1.0 if there is an obstacle on point and otherwise 0.0 
+            if (obstacle == 0)
+            {
+                return false;
+            }
+            return true;
         }
     }
 }
