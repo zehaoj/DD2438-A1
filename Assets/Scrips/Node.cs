540c48075a1474e3ec40d2f2debd2184dc6c7d49
santor <sassator95@hotmail.com>
2022-02-10 19:02:42 +0100

    Fixes after merge

--- Assets/Scrips/Node.cs
+++ Assets/Scrips/Node.cs
@@ -3,36 +3,11 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Data;
+
 
 namespace Scrips
 {
     public class Node<T> : IEqualityComparer, IEnumerable<T>, IEnumerable<Node<T>>
-    {
-        public Node<T> Parent { get; private set; }
-        public T Value { get; set; }
-        public float Cost;
-        private readonly List<Node<T>> _children = new List<Node<T>>();
-
-        public Node(T value)
-        {
-            Value = value;
-        }
-
-        public Node<T> this[int index]
-        {
-            get { return _children[index]; }
-        }
-
-        public Node<T> Add(T value, int index = -1, float cost = 0)
-        {
-            var childNode = new Node<T>(value);
-            Add(childNode, index);
-            childNode.Cost = cost;
-            return childNode;
-        }
-
-        public void Add(Node<T> childNode, int index = -1)
         {
           public Node<T> Parent { get; private set; }
           public T Value { get; set; }
@@ -69,21 +44,21 @@ namespace Scrips
               }
               if (index > Children.Count()-1)
               {
-                  throw new ArgumentException("The index ({0}) can not be higher then index of the last iten. Use the AddChild() method without an index to add at the end".FormatInvariant(index));
+                  throw new ArgumentException($"The index ({index}) can not be higher then index of the last iten. Use the AddChild() method without an index to add at the end");
               }
               if (!childNode.IsRoot)
               {
-                  throw new ArgumentException("The child node with value [{0}] can not be added because it is not a root node.".FormatInvariant(childNode.Value));
+                  throw new ArgumentException($"The child node with value [{childNode.Value}] can not be added because it is not a root node.");
               }
    
               if (Root == childNode)
               {
-                  throw new ArgumentException("The child node with value [{0}] is the rootnode of the parent.".FormatInvariant(childNode.Value));
+                  throw new ArgumentException($"The child node with value [{childNode.Value}] is the rootnode of the parent.");
               }
    
               if (childNode.SelfAndDescendants.Any(n => this == n))
               {
-                  throw new ArgumentException("The childnode with value [{0}] can not be added to itself or its descendants.".FormatInvariant(childNode.Value));
+                  throw new ArgumentException($"The childnode with value [{childNode.Value}] can not be added to itself or its descendants.");
               }
               childNode.Parent = this;
               if (index == -1)
@@ -142,7 +117,7 @@ namespace Scrips
           {
               if (!IsRoot)
               {
-                  throw new ArgumentException("This node [{0}] already has a parent".FormatInvariant(Value), "parentNode");
+                  throw new ArgumentException($"This node [{Value}] already has a parent", "parentNode");
               }
               parentNode.Add(this);
           }
@@ -286,7 +261,7 @@ namespace Scrips
           {
               if (IsRoot)
               {
-                  throw new InvalidOperationException("The root node [{0}] can not get disconnected from a parent.".FormatInvariant(Value));
+                  throw new InvalidOperationException($"The root node [{Value}] can not get disconnected from a parent.");
               }
               Parent._children.Remove(this);
               Parent = null;
@@ -326,7 +301,7 @@ namespace Scrips
               T itemWithIdAndParentIdIsTheSame = valuesCache.FirstOrDefault(v => IsSameId(idSelector(v), parentIdSelector(v)));
               if (itemWithIdAndParentIdIsTheSame != null) // Hier verwacht je ook een null terug te kunnen komen
               {
-                  throw new ArgumentException("At least one value has the samen Id and parentId [{0}]".FormatInvariant(itemWithIdAndParentIdIsTheSame));
+                  throw new ArgumentException($"At least one value has the samen Id and parentId [{itemWithIdAndParentIdIsTheSame}]");
               }
    
               var nodes = valuesCache.Select(v => new Node<T>(v));
@@ -342,7 +317,8 @@ namespace Scrips
               var duplicates = rootNodesCache.Duplicates(n => n).ToList();
               if (duplicates.Any())
               {
-                  throw new ArgumentException("One or more values contains {0} duplicate keys. The first duplicate is: [{1}]".FormatInvariant(duplicates.Count, duplicates[0]));
+                  throw new ArgumentException(
+                      $"One or more values contains {duplicates.Count} duplicate keys. The first duplicate is: [{duplicates[0]}");
               }
                
               foreach (var rootNode in rootNodesCache)
@@ -371,59 +347,59 @@ namespace Scrips
           }
    
           #region Equals en ==
-
-        public static bool operator ==(Node<T> value1, Node<T> value2)
-        {
-            if ((object)(value1) == null && (object)value2 == null)
-            {
-                return true;
-            }
-            return ReferenceEquals(value1, value2);
-        }
-
-        public static bool operator !=(Node<T> value1, Node<T> value2)
-        {
-            return !(value1 == value2);
-        }
-
-        public override bool Equals(Object anderePeriode)
-        {
-            var valueThisType = anderePeriode as Node<T>;
-            return this == valueThisType;
-        }
-
-        public bool Equals(Node<T> value)
-        {
-            return this == value;
-        }
-
-        public bool Equals(Node<T> value1, Node<T> value2)
-        {
-            return value1 == value2;
-        }
-
-        bool IEqualityComparer.Equals(object value1, object value2)
-        {
-            var valueThisType1 = value1 as Node<T>;
-            var valueThisType2 = value2 as Node<T>;
-
-            return Equals(valueThisType1, valueThisType2);
-        }
-
-        public int GetHashCode(object obj)
-        {
-            return GetHashCode(obj as Node<T>);
-        }
-
-        public override int GetHashCode()
-        {
-            return GetHashCode(this);
-        }
-
-        public int GetHashCode(Node<T> value)
-        {
-            return base.GetHashCode();
-        }
+   
+          public static bool operator ==(Node<T> value1, Node<T> value2)
+          {
+              if ((object)(value1) == null && (object) value2 == null)
+              {
+                  return true;
+              }
+              return ReferenceEquals(value1, value2);
+          }
+   
+          public static bool operator !=(Node<T> value1, Node<T> value2)
+          {
+              return !(value1 == value2);
+          }
+   
+          public override bool Equals(Object anderePeriode)
+          {
+              var valueThisType = anderePeriode as Node<T>;
+              return this == valueThisType;
+          }
+   
+          public bool Equals(Node<T> value)
+          {
+              return this == value;
+          }
+   
+          public bool Equals(Node<T> value1, Node<T> value2)
+          {
+              return value1 == value2;
+          }
+   
+          bool IEqualityComparer.Equals(object value1, object value2)
+          {
+              var valueThisType1 = value1 as Node<T>;
+              var valueThisType2 = value2 as Node<T>;
+   
+              return Equals(valueThisType1, valueThisType2);
+          }
+   
+          public int GetHashCode(object obj)
+          {
+              return GetHashCode(obj as Node<T>);
+          }
+   
+          public override int GetHashCode()
+          {
+              return GetHashCode(this);
+          }
+   
+          public int GetHashCode(Node<T> value)
+          {
+              return base.GetHashCode();
+          }
           #endregion
     }
 }
